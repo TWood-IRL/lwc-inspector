f3965b2bddc9c7d81755ed155202b413964f1d9f
Thomas Woodhouse <thomaswoodhouse93@gmail.com>
2020-12-07 15:44:12 +0000

    working oauth2

---
+++ src/client/modules/app/main/main.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class Main extends LightningElement {
+    sessionId = '';
+    myDomainUrl = '';
+    connectedCallback() {}
+}
---
+++ src/server/api.js
@@ -0,0 +1,116 @@
+// Simple Express server setup to serve for local testing/dev API server
+const compression = require('compression');
+const helmet = require('helmet');
+const express = require('express');
+
+// To enable server-side sessions
+const session = require('express-session');
+
+// JSForce
+const jsforce = require('jsforce');
+
+// OPTIONAL: Logging service. Alternately, can use console.error as well
+// In production, we should write to a file, not just console
+const winston = require('winston');
+require('dotenv').config();
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.simple()
+        })
+    ]
+});
+
+// Import Salesforce Authentication modules
+const AuthenticationService = require('./authenticationService');
+
+// Load .env configuration file
+
+const app = express();
+app.use(helmet());
+app.use(compression());
+
+//Retrieve Configuration
+const HOST = process.env.API_HOST || 'localhost';
+const PORT = process.env.API_PORT || 3002;
+const {
+    SALESFORCE_CLIENT_ID,
+    SALESFORCE_CLIENT_SECRET,
+    SALESFORCE_CALLBACK_URL,
+    LOGIN_URL_SANDBOX,
+    LOGIN_URL_PROD,
+    NODE_SESSION_SECRET_KEY
+} = process.env;
+
+// Initialize OAuth2 config
+//const oauth2 = {};
+
+// Initialize Auth Services
+let authService = {};
+
+//Enable server-side sessions
+app.use(
+    session({
+        secret: NODE_SESSION_SECRET_KEY,
+        cookie: { secure: 'auto' },
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
+app.get('/api/v1/endpoint', (req, res) => {
+    res.json({ success: true });
+});
+
+// Hook up REST endpoints with service calls
+
+// Login to Salesforce ///http://localhost:3002/oauth2/login
+app.get('/oauth2/login', (req, res) => {
+    if (authService !== {}) {
+        //spawn a new connection
+        let varLoginURL =
+            req.query.login_type === 'SANDBOX'
+                ? LOGIN_URL_SANDBOX
+                : LOGIN_URL_PROD; //defaulting to prod
+        authService = new AuthenticationService(
+            logger,
+            new jsforce.OAuth2({
+                loginUrl: varLoginURL,
+                clientId: SALESFORCE_CLIENT_ID,
+                clientSecret: SALESFORCE_CLIENT_SECRET,
+                redirectUri: SALESFORCE_CALLBACK_URL
+            })
+        );
+    }
+
+    authService.redirectToAuthUrl(res);
+});
+
+// Callback function to get Auth Token
+app.get('/oauth2/callback', (req, res) => {
+    authService.doCallback(req, res);
+});
+
+// Get Logged In User Details
+app.get('/oauth2/whoami', (req, res) => {
+    authService.getLoggedInUserDetails(req, res);
+});
+
+// Logout from Salesforce
+app.get('/oauth2/logout', (req, res) => {
+    authService.doLogout(req, res);
+});
+
+// Get Conference-Session Details
+// app.get('/api/conference-sessions/:id?', (req, res) => {
+//     integrationService.getConferenceSessionDetails(req, res);
+// });
+
+app.listen(PORT, () =>
+    console.log(
+        `✅  API Server started: http://${HOST}:${PORT}/api/v1/endpoint`
+    )
+);
