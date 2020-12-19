ad6ae34b44758906fd4d4c65c790b1a3ac68feae
Thomas Woodhouse <thomaswoodhouse93@gmail.com>
2020-12-19 04:40:01 +0000

    fixing footer

--- src/client/modules/my/footer/footer.js
+++ src/client/modules/my/footer/footer.js
@@ -4,5 +4,5 @@ import { LABELS } from 'data/labelService';
 export default class Footer extends LightningElement {
     FOOTER_NOTE = LABELS.FOOTER_NOTE;
     GITHUB_URL = LABELS.GITHUB_URL;
-    GITHUB_URL = LABELS.GITHUB_FEEDBACK;
+    GITHUB_FEEDBACK = LABELS.GITHUB_FEEDBACK;
 }
