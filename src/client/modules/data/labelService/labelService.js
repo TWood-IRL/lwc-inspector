a43dce95f1d00bb8c17fc8f2cd0880f34a563361
Thomas Woodhouse <thomaswoodhouse93@gmail.com>
2020-12-29 16:18:45 +0000

    Updating labels + spinner alternative text

--- src/client/modules/data/labelService/labelService.js
+++ src/client/modules/data/labelService/labelService.js
@@ -2,16 +2,17 @@ const LABELS = {
     HEADER: 'Lightning Web Component Inspector',
     LOGIN_PROD: ' Log In Production',
     LOGIN_SANDBOX: ' Log In Sandbox',
-    LOGIN_SESSIONID: 'Use SessionId', 
-    ENTER_SESSION: "Enter SessionId String", 
-    ENTER_MY_DOMAIN: "Enter MyDomainURL", 
-    SET_SESSION: "Set Session Information", 
-    LOGGED_IN_AS: "Currently Logged in as:",
-    WELCOME_HELLO: "Hello ",
-    PLACEHOLDER_SESSION: "12345678..",
-    PLACEHOLDER_MYDOMAIN: "https://mydomain.salesforce.com/", 
-    FOOTER_NOTE:"Created by: Thomas Woodhouse (thomaswoodhouse93@gmail.com", 
-    GITHUB_FEEDBACK: "https://github.com/londoner1234/feedback-lwc-inspector" ,
-    GITHUB_URL:"https://github.com/londoner1234/lwc-inspector"
+    LOGIN_SESSIONID: 'Use SessionId',
+    ENTER_SESSION: 'Enter SessionId String',
+    ENTER_MY_DOMAIN: 'Enter MyDomainURL',
+    SET_SESSION: 'Set Session Information',
+    LOGGED_IN_AS: 'Currently Logged in as:',
+    WELCOME_HELLO: 'Hello ',
+    PLACEHOLDER_SESSION: '12345678..',
+    PLACEHOLDER_MYDOMAIN: 'https://mydomain.salesforce.com/',
+    FOOTER_NOTE: 'Created by: Thomas Woodhouse (thomaswoodhouse93@gmail.com)',
+    GITHUB_FEEDBACK: 'https://github.com/londoner1234/feedback-lwc-inspector',
+    GITHUB_URL: 'https://github.com/londoner1234/lwc-inspector',
+    LOADING_SPINNER: 'Loading...'
 };
 export { LABELS };
