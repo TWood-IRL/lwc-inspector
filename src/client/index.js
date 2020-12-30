783928068e7a16d7dd7b49bb8b3e0e25b47745c2
Thomas Woodhouse <thomaswoodhouse93@gmail.com>
2020-12-30 02:45:47 +0000

    just using npm package for gtm

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,22 @@
 import { createElement } from 'lwc';
 //import MyApp from 'my/app';
 import MainApp from 'my/main';
-import GTM from 'gtm/snippet';
+import Analytics from 'analytics';
+import googleTagManager from '@analytics/google-tag-manager';
 
 import '@lwc/synthetic-shadow'; //allow lightning design system
 //Note - https://github.com/salesforce/lwc/issues/2029 - slots wont work correctly.
 
 const app = createElement('my-app', { is: MainApp });
+const analytics = Analytics({
+    app: 'lwc-inspector',
+    plugins: [
+        googleTagManager({
+            containerId: 'GTM-W4JMV84'
+        })
+    ]
+});
+analytics.page();
+
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
-document.querySelector('#gtmSnippet').src=GTM ; 
